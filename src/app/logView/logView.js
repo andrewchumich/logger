82c3fe101f2f1eaf7b0e6bb488c337403f92d5d1
Andrew Chumich <andrewchumich@gmail.com>
2014-07-16 13:05:56 -0600

    Refactored log view page. Previous week and following week buttons now work

--- src/app/logView/logView.js
+++ src/app/logView/logView.js
@@ -8,7 +8,7 @@ angular.module( 'ngBoilerplate.logView', [
 
 .config(function config( $stateProvider ) {
   $stateProvider.state( 'logView', {
-    url: '/logView/:year/:month/:day',
+    url: '/logView',
     views: {
       "main": {
         controller: 'LogViewCtrl',
@@ -21,13 +21,13 @@ angular.module( 'ngBoilerplate.logView', [
 })
 
 .filter('dateFilter', function() {
-  return function (objects, params, days_ahead) {
+  return function (objects, dateObject, days_ahead) {
       var dateList = [];
       days_ahead = (typeof days_ahead === "undefined") ? 7 : days_ahead;
-      console.log(days_ahead);
+      console.log(dateObject);
       if(objects != null) {
         for(var i = 0; i < objects.length; i++){
-          var today = new Date(params.year, params.month - 1, params.day).getTime();
+          var today = dateObject.getTime();
           var seven_days_ahead = today + days_ahead*24*60*60*1000;
           var date = [objects[i].metrics.date.substring(0,4),objects[i].metrics.date.substring(5,7),objects[i].metrics.date.substring(8,10)];
           var object_date = new Date(date[0], date[1]-1, date[2]).getTime();
@@ -42,7 +42,28 @@ angular.module( 'ngBoilerplate.logView', [
 
 .controller( 'LogViewCtrl', function LogViewCtrl( $scope, $firebase, $stateParams ) {
   // This is simple a demo for UI Boostrap.
-  $scope.params = $stateParams;
+  $scope.beginningOfWeek = new Date();
+  $scope.changeDay = function(number) {
+    $scope.beginningOfWeek.setDate($scope.beginningOfWeek.getDate() + number);
+  };
+  $scope.getDayOfWeek = function() {
+    switch($scope.beginningOfWeek.getDay()) {
+      case 0:
+        return 'Sunday';
+      case 1:
+        return 'Monday';        
+      case 2:
+        return 'Tuesday';        
+      case 3:
+        return 'Wednesday';        
+      case 4:
+        return 'Thursday';         
+      case 5:
+        return 'Friday';  
+      case 6:
+        return 'Saturday';
+    }
+  };
   $scope.log = $firebase(new Firebase('runninglog.firebaseio.com/users/12345/userLogs/'));
   $scope.dropdownDemoItems = [
     "The first choice!",
