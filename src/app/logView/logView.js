d35ce5c4c3cc868cec7d4a33f10595524179088e
Andrew Chumich <andrewchumich@gmail.com>
2014-07-21 14:52:08 -0600

    filter working with objects

--- src/app/logView/logView.js
+++ src/app/logView/logView.js
@@ -24,21 +24,16 @@ angular.module( 'ngBoilerplate.logView', [
 .filter('dateFilter', function() {
   return function (objects, dateObject, days_ahead) {
       var dateList = [];
-      console.log("IM IN HERE: "+objects);
       days_ahead = (typeof days_ahead === "undefined") ? 7 : days_ahead;
-
       if(objects != null && dateObject != null) {
-        for(var i = 0; i < objects.length; i++){
+        for(var object in objects){
           var today = dateObject.getTime() - 24*60*60*1000;
           var seven_days_ahead = today + (days_ahead - 1)*24*60*60*1000;
-          var date = [objects[i].metrics.date.substring(0,4),objects[i].metrics.date.substring(5,7),objects[i].metrics.date.substring(8,10)];
+          var date = [objects[object].metrics.date.substring(0,4),objects[object].metrics.date.substring(5,7),objects[object].metrics.date.substring(8,10)];
           var object_date = new Date(date[0], date[1]-1, date[2]).getTime();
-          console.log('SEVEN DAYS AHEAD: '+seven_days_ahead);
-          console.log('Object Date: '+object_date);
-          console.log('TODAY: '+today);
           
           if (seven_days_ahead >= object_date && today < object_date) {
-            dateList.push(objects[i]);
+            dateList.push(objects[object]);
           }
         }
       }
@@ -50,7 +45,16 @@ angular.module( 'ngBoilerplate.logView', [
   // This is simple a demo for UI Boostrap.
   $scope.log = $firebase(new Firebase('runninglog.firebaseio.com/users/12345/userLogs/running'));
   $scope.beginningOfWeek = new Date();
-  $scope.beginningOfWeek.setDate($scope.beginningOfWeek.getUTCDate() - $scope.beginningOfWeek.getUTCDay() + 1);
+  /*
+    weeks start on Mondays (this will be variable eventually), so the default page, if it is
+    currently a Sunday, should be the previous Monday
+  */
+  if($scope.beginningOfWeek.getUTCDay() !== 0){
+    $scope.beginningOfWeek.setDate($scope.beginningOfWeek.getUTCDate() - $scope.beginningOfWeek.getUTCDay() + 1);
+  }
+  else {
+    $scope.beginningOfWeek.setDate($scope.beginningOfWeek.getUTCDate() - 6);
+  }
   $scope.changeDay = function(number) {
     $scope.beginningOfWeek.setDate($scope.beginningOfWeek.getUTCDate() + number);
   };
